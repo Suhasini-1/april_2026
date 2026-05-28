4a0bb5db3fc336581c4e770006ebd4c1dbb6541f
Suhasini-1 <asuhasini10@gmail.com>
2026-05-28 13:19:39 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,55 @@
+# age = 35
+# if age >= 18:
+#     print(f"your age is {age} and you are eligible to vote")
+#     user = input("enter your name : ")
+#     print(user)
+
+# age = int(input("enter the age : "))
+# if age >= 18:
+#      print(f"your age is {age} and you are eligible to vote")
+#      user = input("enter your name : ")
+#      print(user)
+
+# user_name = input("enter your name :")
+# password = input("Enter the password :")
+# if user_name == "sai" and password == "sai@132" :
+#     print(f"welcome {user_name} to the system")
+#     print(f"login successfull") 
+# else:
+#     print("Invalid credentials")
+
+# #Grades
+# grade = int(input("Enter the marks L"))
+# if grade >= 90 and grade <=100:
+#     print(f" your grade is A and your marks are {grade}")
+# elif grade >= 80 and grade < 90:
+#     print(f"your grade is B and your marks are {grade}")
+# else:
+#     print(f"your grade is failed and your marks are {grade}")
+
+# user_name = input(f" Enter the username :")
+# password = input(f"Enter the password :")
+# if user_name == "sunny":
+#     if password == "Sunny@123" :
+#         print(f"welcome {user_name} to the system")
+#         print(f"Login successfull")
+#     else:
+#         print(f"invalid password")
+# else:
+#     print(f"invalid username")
+
+# num_1 = int(input("Enter the number : "))
+# if num_1 % 2 == 0:
+#     print(f"{num_1} is an even number")
+# # else:
+# #     print(f"{num_1} is an odd number")
+# else:
+#     print(f"{num_1} is not an even number")
+
+
+# num_1 = int(input("Enter the number : "))
+# result = "even" if num_1%2 == 0 else "odd"
+# print(f"{num_1} is an {result} number")
+
+num_1 = int(input("Enter the number : "))
+print(f"{num_1} is an even") if num_1%2 == 0 else print(f"{num_1} is an odd number")
